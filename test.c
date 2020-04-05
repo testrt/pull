90de72781da05fdb0dc4eb2350bc258452c7ed00
Lolico Li <534619360@qq.com>
2020-04-05 16:56:28 +0800

    add test c

---
+++ test.c
@@ -0,0 +1,9 @@
+#include "test.h"
+#include <stdio.h>
+int main()
+{
+    say("hello world\n");
+    int a = 1, b = 2;
+    swap(a, b);
+    printf("a=%d, b=%d", a, b);
+}
---
+++ test.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void say(const char *str)
+{
+    printf(str);
+}
+
+void swap(int a,int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
